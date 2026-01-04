d5b493f29b18982bc0729c16d9f7839a90ec7456
LegionsOfTheBlackFlame <vorgot.gainovel@gmail.com>
2026-01-04 08:28:16 +0300

    Minor bug fix

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,14 +1,10 @@
-import sqlite3 from "sqlite3";
-import path from "path";
+import Database from 'better-sqlite3';
+import path from 'path';
 
-const dbPath = path.resolve("server/db.sqlite");
+const dbPath = path.resolve('./db/db.sqlite');
 
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error("Failed to connect to SQLite DB", err);
-  } else {
-    console.log("Connected to SQLite DB");
-  }
+const db = new Database(dbPath, {
+  verbose: console.log
 });
 
 export default db;
