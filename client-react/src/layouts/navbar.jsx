ff1a0c21723a9e51f62aab4dba3c7000f8953bcb
LegionsOfTheBlackFlame <vorgot.gainovel@gmail.com>
2026-01-31 11:54:46 +0300

    added responsive nav

--- client-react/src/layouts/navbar.jsx
+++ client-react/src/layouts/navbar.jsx
@@ -1,11 +1,20 @@
-import { Link } from 'react-router-dom'
+import { useState } from "react";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
  <nav>
   <div >
     <h1 className='brand-logo'>DiveSchool</h1>
-    <ul>
+       <button
+        className="hamburger"
+        aria-label="Toggle menu"
+        aria-expanded={open}
+        onClick={() => setOpen(o => !o)}
+      >
+        ☰
+      </button>
+    <ul className={`nav-links ${open ? "open" : ""}`}>
       <li><a href="/">Home</a></li>
       <li><a href="/about">About</a></li>
     </ul>
