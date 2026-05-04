926d1a38327a56da3fff2fd76e0b20ca01b58a27
Vorgot <vorgot.gainovel@gmail.com>
2026-05-04 22:06:11 +0300

    dual layout announcement:

--- client-react/src/components/sections/Announcement_sect/AnnouncementSection.jsx
+++ client-react/src/components/sections/Announcement_sect/AnnouncementSection.jsx
@@ -25,6 +25,7 @@
  return (
     <section className={`announcement-section ${visible ? "show" : ""}`}>
       <div className="announcement-content">
+        <div className='track'>
         {section.blocks.map(block => {
           if (block.block_type === 'title') {
             return <h2 key={block.id} className="announcement-title">{block.content}</h2>
@@ -37,6 +38,19 @@
           }
           return null;
         })}
+        {section.blocks.map(block => {
+          if (block.block_type === 'title') {
+            return <h2 key={block.id} className="announcement-title-dup">{block.content}</h2>
+          }
+          if (block.block_type === 'text') {
+            return <p key={block.id} className="announcement-text-dup">{block.content}</p>
+          }
+          if (block.block_type === 'image') {
+            return <img key={block.id} src={block.content} alt="" className="announcement-image" />
+          }
+          return null;
+        })}
+        </div>
       </div>
       <p className="close-button" onClick={handleClose}>x</p>
     </section>
