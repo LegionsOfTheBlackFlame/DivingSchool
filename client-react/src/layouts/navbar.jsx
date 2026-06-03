bb459008fbcdf5c7750965103ca2303fb4df08cc
Vorgot <vorgot.gainovel@gmail.com>
2026-06-03 14:29:53 +0300

    casing typo at navbar

--- client-react/src/layouts/navbar.jsx
+++ client-react/src/layouts/navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import LanguageSwitcher from "./Languageswitch";
+import LanguageSwitcher from "./LanguageSwitch";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
