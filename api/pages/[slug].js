afc3ee880a0f8a226541cee560ced06883a168b4
Vorgot <vorgot.gainovel@gmail.com>
2026-06-06 20:29:29 +0300

    testing vercel logs...

--- api/pages/[slug].js
+++ api/pages/[slug].js
@@ -3,6 +3,7 @@ import { pages } from "../../data/pages.js";
 export default function handler(req, res) {
   const { slug } = req.query;
   const page = pages[slug];
+  console.log('API REQUEST FOR PAGE:', slug, 'FOUND:', !!page)
 
   if (!page) {
     return res.status(404).json({ error: "Page not found" });
