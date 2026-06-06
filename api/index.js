2eff9868bdd983a6901cb5e4b649451eaf4bebf6
Vorgot <vorgot.gainovel@gmail.com>
2026-06-06 20:44:19 +0300

    trying serverless http

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import serverless from "serverless-http";
-import app from "../app.js";
+import app from './server/app.js';
 
 export default serverless(app);
+//this is to get the commit through...
