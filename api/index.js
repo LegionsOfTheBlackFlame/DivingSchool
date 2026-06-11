20b5e8ec3ad5e7a6cdcd3c855637945bd8f6a507
Vorgot <vorgot.gainovel@gmail.com>
2026-06-11 16:25:16 +0300

    troubleshooting vercel entrypoint

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import serverless from "serverless-http";
-import app from './server/app.js';
+import app from '../../server/app.js';
 consol.log('LOADED API INDEX.JS')
 export default serverless(app);
 //this is to get the commit through...
--- api/pages/[slug].js
+++ api/pages/[slug].js
@@ -1,5 +1,5 @@
 import express from "express";
-import app from "../server/app.js";
+import app from "../../server/app.js";
 
 export default function handler(req, res) {
   return app(req, res);
