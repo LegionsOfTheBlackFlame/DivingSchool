f230ae56ec902b00a1a61f85346892bc87dacbac
Vorgot <vorgot.gainovel@gmail.com>
2026-06-11 16:08:44 +0300

    troubleshooting vercel entrypoint

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-import serverless from "serverless-http";
-import app from './server/app.js';
-consol.log('LOADED API INDEX.JS')
-export default serverless(app);
-//this is to get the commit through...
+import app from '../server/app.js';
+
+export default app;
